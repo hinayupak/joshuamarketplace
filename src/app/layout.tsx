1f20cda87261daf819188c0368e7cb070baf4d61
Joshua Hernandez <joshua.hernandez.ph@gmail.com>
2024-03-09 22:00:02 +0800

    nextjs waring extra attributes from server fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
-      <body className={cn("relative h-full font-sans antialiased", inter.className)}>
+    <html lang="en" className="h-full" suppressHydrationWarning={true}>
+      <body
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+        suppressHydrationWarning={true}
+      >
         <main className="relative flex flex-col min-h-screen">
           <Navbar />
           <div className="flex-grow flex-1">{children}</div>
